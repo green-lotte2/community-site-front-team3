9320ee082de7e282a6570f17202393eee3b23ed5
gudals <gudals9677@gmail.com>
2024-05-22 15:38:33 +0900

    321

--- orangenode/src/router/root.js
+++ orangenode/src/router/root.js
@@ -12,17 +12,17 @@ import RegisterPage from '../pages/member/RegisterPage';
 import ProjectBoardPage from 'pages/project/ProjectBoardPage';
 import ProjectListPage from 'pages/project/ProjectListPage';
 import List from 'pages/board/List';
-import Modify from 'pages/board/Modify';
-import Register from 'pages/board/Register';
-import View from 'pages/board/View';
-import NewPage from 'pages/newPage/NewPage';
+import Modify from 'pages/board/modify';
+import Register from 'components/member/Register';
+import View from 'pages/board/view';
+import NewPage from 'pages/newPage/newPage';
 
 // 라우터 생성
 const root = createBrowserRouter([
     // main
     { path: '/', element: <MainPage /> },
-  
-     // admin
+
+    // admin
     { path: '/admin', element: <AdminPage /> },
     { path: '/admin/member', element: <AdminMember /> },
     { path: '/admin/member/list', element: <AdminMemberList /> },
