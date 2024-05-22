2082ba9f9013f459495c49ad91f5bd476e504e3b
gudals <gudals9677@gmail.com>
2024-05-22 17:03:26 +0900

    css

--- orangenode/src/components/member/Login.js
+++ orangenode/src/components/member/Login.js
@@ -7,14 +7,8 @@ const Login = () => {
         <>
             <div className="container">
                 <div className="login-container">
-                    <div className="login-options">
-                        <div className="login-buttons">
-                            <button className="kakao-login"></button>
-                            <button className="google-login"></button>
-                        </div>
-                    </div>
                     <div className="login-form">
-                        <form>
+                        <form className="login-form-submit">
                             <label htmlFor="user-id">아이디</label>
                             <input type="text" id="user-id" name="user-id" placeholder="Enter your ID" />
 
@@ -27,6 +21,11 @@ const Login = () => {
                             </div>
 
                             <button type="submit">로그인</button>
+
+                            <div className="login-buttons">
+                                <button className="kakao-login"></button>
+                                <button className="google-login"></button>
+                            </div>
                         </form>
                     </div>
                 </div>
