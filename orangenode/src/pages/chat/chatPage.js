7c861d9b5c56bf91317e96b9cdbc7c81b496ad75
ty <qksrm5546@gmail.com>
2024-05-22 11:45:43 +0900

    aaa

---
+++ orangenode/src/pages/chat/chatPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ChatLayout from '../../layouts/ChatLayout';
+import Chat from './chatPage'
+
+export const Chatpage = () => {
+    return (
+        <ChatLayout>
+            <Chat />
+        </ChatLayout>
+    );
+};
