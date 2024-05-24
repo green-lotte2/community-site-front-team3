850287043e79bbdcd268c941d0fe239a88f40a1e
JO9 <sardia0923@gmail.com>
2024-05-24 12:36:47 +0900

    add

--- src/components/common/admin/Aside.js
+++ src/components/common/admin/Aside.js
@@ -7,22 +7,22 @@ const Aside = () => {
             <aside>
                 <ul>
                     <li>
-                        <img src="/images/icon/headset.png" alt="헤드셋" style={{ width: '15px' }} />
+                        <img src="/images/icon/home.png" alt="헤드셋" style={{ width: '15px' }} />
                         <Link to="/admin">HOME</Link>
                     </li>
                     <li>
-                        <img src="/images/icon/headset.png" alt="헤드셋" style={{ width: '15px' }} />
+                        <img src="/images/icon/article.png" alt="게시판" style={{ width: '15px' }} />
                         <Link to="/admin/article">게시판</Link>
                     </li>
                     <li>
+                        <img src="/images/icon/user2.png" alt="회원" style={{ width: '15px' }} />
                         <Link to="/admin/member">
                             <i aria-hidden="true"></i>회원관리
                         </Link>
-                        <img src="/images/icon/headset.png" alt="헤드셋" style={{ width: '15px' }} />
                         <Link to="/admin/member/list"></Link>
                     </li>
                     <li>
-                        <img src="/images/icon/headset.png" alt="헤드셋" style={{ width: '15px' }} />
+                        <img src="/images/icon/headset.png" alt="CS" style={{ width: '15px' }} />
                         <Link to="#">고객센터</Link>
                     </li>
                 </ul>
