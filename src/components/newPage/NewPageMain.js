e185e479d6fae753e907f0801084ca4716921d1e
linma21 <154877422+linma21@users.noreply.github.com>
2024-05-26 10:44:09 +0900

    Page front

--- src/components/newPage/NewPageMain.js
+++ src/components/newPage/NewPageMain.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import EditableBlock from './EditableBlock';
 
 const NewPageMain = () => {
+  // EditableBlock에 보낼 props
+  const [block, setBlock] = useState({ contents: "" });
+
+  /** 내용 입력할 때마다 EditableBlock에 보낼 props 수정 */
+  const handleBlockChange = (newContents) => {
+    setBlock({ contents: newContents });
+  };
+
   return (
     <div className="NewPageMain">
-    <h2>새 타이틀입니다.</h2>
-    <p>내용을 입력하세요.</p>
-    </div>
-  )
-}
+      <h1 class="pageTitle" spellcheck="true" placeholder="제목 없음" contentEditable="true"></h1>
+      <EditableBlock block={block} onChange={handleBlockChange} />
 
-export default NewPageMain
+      
+    </div>
+  );
+};
+
+export default NewPageMain;
