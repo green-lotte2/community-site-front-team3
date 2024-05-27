3f3f715ab6399464427e6032f08a5f5a375f0a8f
Taeyoung20230727 <140632598+Taeyoung20230727@users.noreply.github.com>
2024-05-27 18:04:53 +0900

    Update ChatInput.js

--- src/components/chat/ChatInput.js
+++ src/components/chat/ChatInput.js
@@ -17,7 +17,6 @@ const ChatInput = ({ onSendMessage }) => {
         type="text"
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
-        placeholder="Type your message here..."
       />
       <button type="submit">SEND</button>
     </form>
