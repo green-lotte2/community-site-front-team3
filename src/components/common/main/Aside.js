f55aef727e275288348f3ef48c3c45a39a32480a
gudals <gudals9677@gmail.com>
2024-05-29 17:32:23 +0900

    프로젝트 생성

--- src/components/common/main/Aside.js
+++ src/components/common/main/Aside.js
@@ -1,55 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FaCalendarAlt, FaClipboardList } from "react-icons/fa";
-import { IoChatboxEllipses } from "react-icons/io5";
-import { MdContactPage } from "react-icons/md";
-import { RiCustomerServiceFill } from "react-icons/ri";
-import { AiFillProject } from "react-icons/ai";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaCalendarAlt, FaClipboardList } from 'react-icons/fa';
+import { IoChatboxEllipses } from 'react-icons/io5';
+import { MdContactPage } from 'react-icons/md';
+import { RiCustomerServiceFill } from 'react-icons/ri';
+import { AiFillProject } from 'react-icons/ai';
 const Aside = () => {
-  return (
-    <>
-      <aside>
-        <ul>
-          <li>
-            <Link to="/project/board">
-              <AiFillProject />
-              프로젝트
-            </Link>
-          </li>
-          <li>
-            <Link to="/calendar">
-              <FaCalendarAlt />
-              캘린더
-            </Link>
-          </li>
-          <li>
-            <Link to="/page">
-              <MdContactPage />
-              페이지
-            </Link>
-          </li>
-          <li>
-            <Link to="/board/list">
-              <FaClipboardList />
-              게시판
-            </Link>
-          </li>
-          <li>
-            <Link to="/chat">
-              <IoChatboxEllipses />
-              채팅
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <RiCustomerServiceFill />
-              고객센터
-            </Link>
-          </li>
-        </ul>
-      </aside>
-    </>
-  );
+    return (
+        <>
+            <aside>
+                <ul>
+                    <li>
+                        <Link to="/project/board">
+                            <AiFillProject />
+                            프로젝트
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/calendar">
+                            <FaCalendarAlt />
+                            캘린더
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/page">
+                            <MdContactPage />
+                            페이지
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/board/list">
+                            <FaClipboardList />
+                            게시판
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/chat">
+                            <IoChatboxEllipses />
+                            채팅
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#">
+                            <RiCustomerServiceFill />
+                            고객센터
+                        </Link>
+                    </li>
+                </ul>
+            </aside>
+        </>
+    );
 };
 
 export default Aside;
--- src/components/project/ProjectList.js
+++ src/components/project/ProjectList.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
+import moment from 'moment';
 
 const ProjectList = () => {
     const authSlice = useSelector((state) => state.authSlice);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [projectTitle, setProjectTitle] = useState('');
     const [invitedUsers, setInvitedUsers] = useState([]);
+    const [userUids, setuserUids] = useState([]);
     const [users, setUsers] = useState([]);
-    const [userCompany, setUserCompany] = useState('');
+    const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        if (authSlice && authSlice.user && authSlice.company) {
-            console.log(authSlice.company);
-
-            const data = fetchUsersByCompany(authSlice.company);
-            console.log(data);
+        if (authSlice && authSlice.uid && authSlice.company) {
+            fetchUsersByCompany(authSlice.company);
+            selectProjectList();
         }
     }, [authSlice]);
 
@@ -27,6 +27,31 @@ const ProjectList = () => {
             setUsers(response.data);
         } catch (error) {
             console.error('Error fetching users:', error);
+        }
+    };
+    // 프로젝트 리스트 출력 //
+    const selectProjectList = async () => {
+        try {
+            const response = await axios.get(`/project/list?uid=${authSlice.uid}`);
+            setProjects(response.data);
+        } catch (error) {
+            console.error('Error fetching projects:', error);
+        }
+    };
+
+    // 프로젝트 생성 //
+    const createProject = async (e) => {
+        try {
+            e.preventDefault();
+
+            const response = await axios.post(`/project/create`, {
+                uid: authSlice.uid,
+                title: projectTitle,
+                uids: userUids,
+            });
+            console.log('Project created:', response.data.proNo);
+        } catch (error) {
+            console.error('Error creating project:', error);
         }
     };
 
@@ -43,15 +68,27 @@ const ProjectList = () => {
     };
 
     const handleInviteUser = (e) => {
-        const selectedUser = e.target.value;
-        setInvitedUsers([...invitedUsers, selectedUser]);
+        const selectdata = e.target.value.split('?');
+        const selectedUser = selectdata[0];
+
+        const selectUids = selectdata[1];
+
+        if (selectedUser && !invitedUsers.includes(selectedUser)) {
+            setInvitedUsers([...invitedUsers, selectedUser]);
+        }
+        if (selectUids && !selectUids.includes(selectedUser)) {
+            setuserUids([...userUids, selectUids]);
+        }
     };
 
-    const handleCreateProject = () => {
-        console.log('Project created:', projectTitle, invitedUsers);
+    const handleCreateProject = (e) => {
+        createProject(e);
         closeModal();
     };
 
+    const getFilteredUsers = () => {
+        return users.filter((user) => !invitedUsers.includes(user.uid));
+    };
     return (
         <>
             <div className="project-list-container">
@@ -63,16 +100,22 @@ const ProjectList = () => {
                             New Project
                         </button>
                     </div>
-                    <div className="project-card">
-                        <p>프로젝트 제목</p>
-                        <div className="project-meta">
-                            <span className="date">프로젝트 생성일</span>
-                            <span className="actions">
-                                <button>Edit</button>
-                                <button>Delete</button>
-                            </span>
-                        </div>
-                    </div>
+                    {projects.length > 0 ? (
+                        projects.map((project, index) => (
+                            <div className="project-card" key={index}>
+                                <p>{project.title}</p>
+                                <div className="project-meta">
+                                    <span className="date">{moment(projects.rdate).format('YY.MM.DD')}</span>
+                                    <span className="actions">
+                                        <button>Edit</button>
+                                        <button>Delete</button>
+                                    </span>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No projects found</p>
+                    )}
                 </div>
             </div>
             {/* 모달 */}
@@ -89,8 +132,8 @@ const ProjectList = () => {
                         <select id="inviteUser" onChange={handleInviteUser}>
                             <option value="">Select User</option>
                             {/* 사용자 정보 출력 */}
-                            {users.map((user) => (
-                                <option key={user.id} value={user.id}>
+                            {getFilteredUsers().map((user, index) => (
+                                <option key={index} value={user.name + '?' + user.uid}>
                                     {user.name}
                                 </option>
                             ))}
