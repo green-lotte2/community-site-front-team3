822322c4863fe46aa1f466748f9d715d7f2dd337
linma21 <154877422+linma21@users.noreply.github.com>
2024-05-29 09:07:56 +0900

    메인 페이지 수정

--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import DefaultLayout from '../../layouts/DefaultLayout';
 import Main from '../../components/main/Main';
-import MonthCalendar from 'components/calendar/MonthCalendar';
 
 const MainPage = () => {
     return (
         <DefaultLayout>
             <Main />
-            <div className="calendar">
-                <MonthCalendar />
-                
-            </div>
         </DefaultLayout>
     );
 };
