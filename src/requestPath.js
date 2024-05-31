c33f9e478035d8c7c71735f47e8e9ca66ad1b9b2
gudals <gudals9677@gmail.com>
2024-05-31 11:18:08 +0900

    프로젝트 삭제

--- src/requestPath.js
+++ src/requestPath.js
@@ -1,11 +1,8 @@
 import { globalPath } from 'globalPaths';
 const path = globalPath.path;
 
-
 /* 관리자 */
 export const ADMIN_ARTICLE_LIST_PATH = path + '/admin/article';
-
-
 
 /* 회원 */
 export const MAIN_PATH = path + '/main';
@@ -16,7 +13,10 @@ export const TERMS_PATH = path + '/user/terms';
 export const CHECK_EMAIL_CODE_PATH = path + '/member/checkEmailCode';
 export const SEND_EMAIL_CODE_PATH = path + '/member/checkUser/email';
 
-
-
 /* 프로젝트 */
 export const PROJECT_KANBAN_PATH = path + '/project/kanban';
+export const PROJECT_SELECT_PATH = path + '/user/company?company';
+export const PROJECT_LIST_PATH = path + '/project/list?uid';
+export const PROJECT_CREATE_PATH = path + '/project/create';
+export const PROJECT_UPDATE_PATH = path + '/project/update';
+export const PROJECT_DELETE_PATH = path + '/project/delete?proNo';
