c3c77ec8dbaed494733f1af2338804b4677af127
sun-gwang <kkj80911@gmail.com>
2024-06-03 17:40:44 +0900

    add

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import Updatess from './Updatess';
+import React from "react";
+import Recents from "./Recents";
+import MainCalendar from "./MainCalendar";
+import MainPages from "./MainPages";
+import MainChat from "./MainChat";
+import MainArticles from "./MainArticles";
+import MainCs from "./MainCs";
+import MainProj from "./MainProj";
+import { useSelector } from "react-redux";
 const Main = () => {
-import Recents from './Recents';
-import MainCalendar from './MainCalendar';
-import MainPages from './MainPages';
-import MainChat from './MainChat';
-import MainArticles from './MainArticles';
-import MainCs from './MainCs';
-import MainProj from './MainProj';
-import MainEventList from './MainEventList'; // EventList 컴포넌트를 가져옵니다.
-import { useSelector } from 'react-redux';
-const Main = () => {
-    const authSlice = useSelector((state) => state.authSlice);
-    console.log('main:', authSlice);
-    return (
-        <>
-            <Recents />
-            <MainCalendar />
-            <MainProj />
-            <MainPages />
-            <MainChat />
-            <MainArticles />
-            <MainCs />
-        </>
-    );
+  const authSlice = useSelector((state) => state.authSlice);
+  console.log("main:", authSlice);
+  return (
+    <>
+      <Recents />
+      <MainCalendar />
+      <MainProj />
+      <MainPages />
+      <MainChat />
+      <MainArticles />
+      <MainCs />
+    </>
+  );
 };
 
 export default Main;
