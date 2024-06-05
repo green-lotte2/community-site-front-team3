8f568ada83c1f39189f0c499ed1d153b844b4088
ty <qksrm5546@gmail.com>
2024-06-05 17:20:41 +0900

    채팅 name띄우기

--- src/components/chat/ChatMessage.js
+++ src/components/chat/ChatMessage.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Avatar from "@mui/material/Avatar";
-import { deepOrange, deepPurple } from "@mui/material/colors";
+import { deepOrange } from "@mui/material/colors";
 
 const ChatMessage = ({ position, text, name }) => {
+  console.log("name" + name);
   return (
     <div className={`chat-message ${position}`}>
       <div className="chat-user">
         <span className="chat-username">{name}</span>
         <Avatar
           sx={{ bgcolor: deepOrange[400] }}
-          alt={name}
           src="../../images/icon/BigOrage.jpg"
         />
       </div>
