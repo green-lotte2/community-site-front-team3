b1b3e9bd9ce1bfcfa52b373845ef94e0824957c5
그럼컴퓨터 롯데2차 과정 <167052369+green-lotte2@users.noreply.github.com>
2024-06-14 10:55:06 +0900

    관리자 페이지 수정 중

--- src/components/admin/main/Container.js
+++ src/components/admin/main/Container.js
@@ -7,7 +7,7 @@ const container = () => {
         <>
             <div className="container">
                 <h2>관리자 메인</h2>
-                <p>Manage your content and projects efficiently</p>
+                <p>관리자 메인 페이지 입니다. 최신 게시글, 최신 회원 등을 확인 할 수 있습니다.</p>
                 <div className="dashboard">
                     <Recent />
                     <Member />
