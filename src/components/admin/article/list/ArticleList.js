f47cc2bba1ee5e82c0eef2909d9ce8224ff4a342
ty <qksrm5546@gmail.com>
2024-06-18 17:16:06 +0900

    게시판 페이지네이션, 유료회원이면 게시판 생성버튼 활성화

--- src/components/admin/article/list/ArticleList.js
+++ src/components/admin/article/list/ArticleList.js
@@ -1,128 +1,142 @@
-import React, { useState } from 'react';
-import moment from 'moment';
-import axios from 'axios';
-import { globalPath } from 'globalPaths';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import moment from "moment";
+import axios from "axios";
+import { globalPath } from "globalPaths";
+import { useNavigate } from "react-router-dom";
 
 const ArticleList = ({ articleList, setArticleList }) => {
-    const [selectedArticles, setSelectedArticles] = useState([]); // 게시글 저장하는 상태
-    const [selectAll, setSelectAll] = useState(false); // 전체 선택 상태를 저장하는 상태
-    const navigate = useNavigate();
-    const url = globalPath.path;
+  const [selectedArticles, setSelectedArticles] = useState([]); // 게시글 저장하는 상태
+  const [selectAll, setSelectAll] = useState(false); // 전체 선택 상태를 저장하는 상태
+  const navigate = useNavigate();
+  const url = globalPath.path;
 
-    // 게시글 전체 선택, selectedArticles 상태를 업데이트
-    const handleSelectAll = () => {
-        if (selectAll) {
-            setSelectedArticles([]);
-        } else {
-            setSelectedArticles(articleList.map((article) => article.ano));
-        }
-        setSelectAll(!selectAll);
-    };
+  // 게시글 전체 선택, selectedArticles 상태를 업데이트
+  const handleSelectAll = () => {
+    if (selectAll) {
+      setSelectedArticles([]);
+    } else {
+      setSelectedArticles(articleList.map((article) => article.ano));
+    }
+    setSelectAll(!selectAll);
+  };
 
-    // 게시글 개별 선택, 선택된 article의 id를 selectedArticles 상태에 추가하거나 제거
-    const handleSelectArticle = (ano) => {
-        setSelectedArticles((prevSelected) =>
-            prevSelected.includes(ano) ? prevSelected.filter((id) => id !== ano) : [...prevSelected, ano]
-        );
-    };
-
-    // 게시글 선택 삭제
-
-    const handleDelSelected = () => {
-        if (window.confirm('선택한 게시글(들)을 삭제하시겠습니까?')) {
-            selectedArticles.forEach((ano) => {
-                axios
-                    .delete(`${url}/admin/article/${ano}`)
-                    .then(() => {
-                        setArticleList((prevList) => prevList.filter((article) => article.ano !== ano));
-                        setSelectedArticles((prevSelected) => prevSelected.filter((id) => id !== ano));
-                    })
-                    .catch((err) => {
-                        console.error('글 삭제 오류:', err);
-                    });
-            });
-            setSelectAll(false);
-        }
-    };
-
-    // 게시글 삭제 버튼
-    const del = (ano) => {
-        if (window.confirm('정말 삭제하시겠습니까?')) {
-            axios
-                .delete(`${url}/admin/article/${ano}`)
-                .then(() => {
-                    setArticleList((prevList) => prevList.filter((article) => article.ano !== ano));
-                })
-                .catch((err) => {
-                    console.error('글 삭제 오류:', err);
-                });
-        }
-    };
-
-    // 게시글 상세보기
-    const view = (ano) => {
-        navigate(`/admin/article/${ano}`);
-    };
-
-    // 게시글 내용 ... 출력
-    const cutContent = (content, maxLength) => {
-        return content.length > maxLength ? content.subString(0, maxLength) + '...' : content;
-    };
-
-    return (
-        <div>
-            <button onClick={handleDelSelected} className="btn-del-selected">
-                선택삭제
-            </button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            전체선택
-                            <input type="checkbox" checked={selectAll} onChange={handleSelectAll} />
-                        </th>
-                        <th>번호</th>
-                        <th>제목</th>
-                        <th>내용</th>
-                        <th>날짜</th>
-                        <th>상태</th>
-                        <th>관리</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {articleList.map((article) => (
-                        <tr key={article.ano}>
-                            <td>
-                                <input
-                                    type="checkbox"
-                                    checked={selectedArticles.includes(article.ano)}
-                                    onChange={() => handleSelectArticle(article.ano)}
-                                />
-                            </td>
-                            <td>{article.ano}</td>
-                            <td>
-                                {article.title}
-                                <br />
-                                <small>{article.uid}</small>
-                            </td>
-                            <td>{cutContent(article.content, 20)}</td>
-                            <td>{moment(article.rdate).format('YY-MM-DD')}</td>
-                            <td>{article.reply}</td>
-                            <td>
-                                <button onClick={() => view(article.ano)} className="btn-view">
-                                    상세보기
-                                </button>
-                                <button onClick={() => del(article.ano)} className="btn-del">
-                                    삭제
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+  // 게시글 개별 선택, 선택된 article의 id를 selectedArticles 상태에 추가하거나 제거
+  const handleSelectArticle = (ano) => {
+    setSelectedArticles((prevSelected) =>
+      prevSelected.includes(ano)
+        ? prevSelected.filter((id) => id !== ano)
+        : [...prevSelected, ano]
     );
+  };
+
+  // 게시글 선택 삭제
+
+  const handleDelSelected = () => {
+    if (window.confirm("선택한 게시글(들)을 삭제하시겠습니까?")) {
+      selectedArticles.forEach((ano) => {
+        axios
+          .delete(`${url}/admin/article/${ano}`)
+          .then(() => {
+            setArticleList((prevList) =>
+              prevList.filter((article) => article.ano !== ano)
+            );
+            setSelectedArticles((prevSelected) =>
+              prevSelected.filter((id) => id !== ano)
+            );
+          })
+          .catch((err) => {
+            console.error("글 삭제 오류:", err);
+          });
+      });
+      setSelectAll(false);
+    }
+  };
+
+  // 게시글 삭제 버튼
+  const del = (ano) => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      axios
+        .delete(`${url}/admin/article/${ano}`)
+        .then(() => {
+          setArticleList((prevList) =>
+            prevList.filter((article) => article.ano !== ano)
+          );
+        })
+        .catch((err) => {
+          console.error("글 삭제 오류:", err);
+        });
+    }
+  };
+
+  // 게시글 상세보기
+  const view = (ano) => {
+    navigate(`/admin/article/${ano}`);
+  };
+
+  // 게시글 내용 ... 출력
+  const cutContent = (content, maxLength) => {
+    return content.length > maxLength
+      ? content.subString(0, maxLength) + "..."
+      : content;
+  };
+
+  return (
+    <div>
+      <button onClick={handleDelSelected} className="btn-del-selected">
+        선택삭제
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              전체선택
+              <input
+                type="checkbox"
+                checked={selectAll}
+                onChange={handleSelectAll}
+              />
+            </th>
+            <th>번호</th>
+            <th>제목</th>
+            <th>내용</th>
+            <th>날짜</th>
+            <th>상태</th>
+            <th>관리</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articleList.map((article) => (
+            <tr key={article.ano}>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={selectedArticles.includes(article.ano)}
+                  onChange={() => handleSelectArticle(article.ano)}
+                />
+              </td>
+              <td>{article.ano}</td>
+              <td>
+                {article.title}
+                <br />
+                <small>{article.uid}</small>
+              </td>
+              <td>{cutContent(article.content, 20)}</td>
+              <td>{moment(article.rdate).format("YY-MM-DD")}</td>
+              <td>{article.reply}</td>
+              <td>
+                <button onClick={() => view(article.ano)} className="btn-view">
+                  상세보기
+                </button>
+                <button onClick={() => del(article.ano)} className="btn-del">
+                  삭제
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ArticleList;
--- src/components/board/BoardContainer.js
+++ src/components/board/BoardContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import BoardTabs from "./BoardTabs";
 import EditorContainer from "./EditorContainer";
 
