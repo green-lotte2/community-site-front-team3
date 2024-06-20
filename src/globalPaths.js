6ff10a37055139f0c3c624416f8a94757e900f9b
gudals <gudals9677@gmail.com>
2024-06-20 17:00:55 +0900

    배포테스트

--- src/globalPaths.js
+++ src/globalPaths.js
@@ -1,19 +1,19 @@
 //const serverHost = '127.0.0.1';
- const serverHost = '3.34.204.24'; 
+const serverHost = '3.34.204.24';
 
 export const globalPath = {
-  serverHost,
-  path: `http://${serverHost}:8080`,
+    serverHost,
+    path: `http://${serverHost}:8080`,
 
-  /* 회원 */
-  mainPath: "/main",
-  user: "/user",
-  regitserPath: "/member/register",
-  terms: "/member/terms",
-  checkEmailCode: "/member/checkEmailCode",
-  sendEmailCode: "/member/checkUser/email",
+    /* 회원 */
+    mainPath: '/main',
+    user: '/user',
+    regitserPath: '/member/register',
+    terms: '/member/terms',
+    checkEmailCode: '/member/checkEmailCode',
+    sendEmailCode: '/member/checkUser/email',
 
-  /* 프로젝트 */
-  projectList: "/project/list",
-  projectKanBan: "/project/kanban",
+    /* 프로젝트 */
+    projectList: '/project/list',
+    projectKanBan: '/project/kanban',
 };
