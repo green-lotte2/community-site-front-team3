aa4a49b0bc9b853a713fc9ce97a9abbafdec99c8
sun-gwang <142211574+sun-gwang@users.noreply.github.com>
2024-06-23 06:06:57 +0900

    내 문의, 관리자 문의관리 css 수정

--- src/components/main/CsModal.js
+++ src/components/main/CsModal.js
@@ -1,10 +1,11 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import React from "react";
-import { Card } from "@mui/material";
+import React, { useState } from "react";
+import { Accordion, Card } from "@mui/material";
 import "../../styles/cs/cs.scss";
 import { globalPath } from "globalPaths";
+import axios from "axios";
 
 const style = {
   position: "absolute",
@@ -21,6 +22,29 @@ const style = {
 const url = globalPath.path;
 
 export default function CsModal({ open, handleClose, myAnswer }) {
+  const [expandedIndex, setExpandedIndex] = useState(""); // 답변보기 관리 state
+
+  const [getAnswer, setGetAnswer] = useState(""); // 가져온 답변 관리 state
+
+  /**답변 가져오기 */
+  const selectAnswer = (e) => {
+    axios
+      .get(`${url}/question/selectAnswer?parent=${e}`)
+      .then((response) => {
+        console.log(response.data);
+        setGetAnswer(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const viewBtnHandler = (e) => {
+    setExpandedIndex(expandedIndex === "접기" ? "답변보기" : "접기");
+    console.log(expandedIndex);
+    console.log(e);
+    /**답변 가져오기 로직 수행 */
+    selectAnswer(e);
+  };
   return (
     <Modal
       open={open}
@@ -28,15 +52,13 @@ export default function CsModal({ open, handleClose, myAnswer }) {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={style}>
+      <Box className="modaldal" sx={style}>
         <Typography
           style={{
-            display: "inline-block",
             fontWeight: "bolder",
             padding: "6px 12px",
             border: "1px solid #FFFFFF",
             borderRadius: "10px",
-            fontWeight: "bold",
             backgroundColor: "#8181F7",
             color: "#FFFFFF",
             textAlign: "center",
@@ -51,20 +73,24 @@ export default function CsModal({ open, handleClose, myAnswer }) {
         {myAnswer
           .filter((answer) => answer.status === 0)
           .map((answer, index) => (
-            <Card style={{ margin: "10px" }}>
-              <Typography
-                key={index}
-                id="modal-modal-description"
-                style={{ mt: 2 }}
-                dangerouslySetInnerHTML={{
-                  __html: answer.content.replace("@FilePath###", url),
-                }}
-              ></Typography>
-            </Card>
+            <div key={index} style={{ margin: "10px" }}>
+              <span style={{}}>
+                {index + 1}. {answer.title}
+              </span>
+              <Card>
+                <Typography
+                  key={index}
+                  id="modal-modal-description"
+                  style={{ mt: 2 }}
+                  dangerouslySetInnerHTML={{
+                    __html: answer.content.replace("@FilePath###", url),
+                  }}
+                ></Typography>
+              </Card>
+            </div>
           ))}
         <Typography
           style={{
-            display: "inline-block",
             fontWeight: "bolder",
             padding: "6px 12px",
             border: "1px solid #FFFFFF",
@@ -74,6 +100,7 @@ export default function CsModal({ open, handleClose, myAnswer }) {
             color: "#FFFFFF",
             textAlign: "center",
             marginLeft: "6px",
+            marginTop: "30px",
           }}
           id="modal-modal-title"
           variant="h6"
@@ -81,18 +108,44 @@ export default function CsModal({ open, handleClose, myAnswer }) {
         >
           답변 완료
         </Typography>
-        {myAnswer
-          .filter((answer) => answer.status === 1)
-          .map((answer, index) => (
-            <Card style={{ margin: "10px" }}>
-              <Typography
-                key={index}
-                id="modal-modal-description"
-                style={{ mt: 2 }}
-                dangerouslySetInnerHTML={{ __html: answer.content }}
-              ></Typography>
-            </Card>
-          ))}
+        {myAnswer && myAnswer.length > 0 ? (
+          myAnswer
+            .filter((answer) => answer.status === 1)
+            .map((answer, index) => (
+              <div key={index} style={{ margin: "10px" }}>
+                <span style={{ margin: "3px" }}>
+                  {index + 1}. {answer.title}
+                </span>
+                <Card>
+                  <Typography
+                    key={index}
+                    id="modal-modal-description"
+                    style={{ mt: 2 }}
+                    dangerouslySetInnerHTML={{
+                      __html: answer?.content ? answer.content : "문의 없음",
+                    }}
+                  ></Typography>
+                </Card>
+                <span
+                  style={{ cursor: "pointer" }}
+                  onClick={() => viewBtnHandler(answer.qno)}
+                >
+                  {expandedIndex === "접기" ? "답변접기" : "답변보기"}
+                </span>
+                {expandedIndex === "접기" && (
+                  <div
+                    style={{
+                      padding: "0 10px",
+                    }}
+                  >
+                    &gt; {getAnswer.content}
+                  </div>
+                )}
+              </div>
+            ))
+        ) : (
+          <div>문의 내역이 없습니다.</div>
+        )}
       </Box>
     </Modal>
   );
